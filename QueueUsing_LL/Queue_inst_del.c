1dddacdce3b6a726e4fdbb351d1e4fa17f811917
Souro Biswas <hopepope911@gmail.com>
2023-08-28 23:26:10 +0530

    queue implementation

--- QueueUsing_LL/Queue_inst_del.c
+++ QueueUsing_LL/Queue_inst_del.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
     struct Node *next;
 };
 
-struct Queue{
+struct Queue
+{
     struct Node *front;
     struct Node *rear;
 };
@@ -17,7 +19,6 @@ int dequeue(struct Queue *);
 void deA_queue(struct Queue *); // deallocates memory
 
 int main()
-
 {
     struct Queue Q;
     int data;
@@ -27,10 +28,6 @@ int main()
     enqueue(&Q, 15);
     enqueue(&Q, 16);
     enqueue(&Q, 17);
-    
-    data = dequeue(&Q);
-    if (data != -1)
-        printf("deleted element: %d\n", data);
 
     data = dequeue(&Q);
     if (data != -1)
@@ -48,19 +45,65 @@ int main()
     if (data != -1)
         printf("deleted element: %d\n", data);
 
-    deA_queue(&Q);
+    data = dequeue(&Q);
+    if (data != -1)
+        printf("deleted element: %d\n", data);
+
+    // deA_queue(&Q);
     return 0;
 }
 
-void initQ(struct Queue *){
-
+void initQ(struct Queue *q)
+{
+    q->front = q->rear = NULL;
 }
-void enqueue(struct Queue *, int){
 
+void enqueue(struct Queue *q, int val)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    if (temp == NULL)
+    {
+        printf("Queue is Full!");
+        return;
+    }
+
+    temp->data = val;
+    temp->next = NULL;
+
+    if (q->front == NULL)
+    {
+        q->rear = q->front = temp;
+        return;
+    }
+
+    q->rear->next = temp;
+    q->rear = temp;
 }
-int dequeue(struct Queue *){
 
+int dequeue(struct Queue *q)
+{
+    struct Node *temp;
+    int val;
+
+    if (q->front == NULL)
+    {
+        printf("Queue is Empty!");
+        return -1;
+    }
+
+    val = q->front->data;
+    temp = q->front;
+    q->front = q->front->next;
+    free(temp);
+    return val;
 }
-void deA_queue(struct Queue *){
 
+void deA_queue(struct Queue *q)
+{
+    while (q->front != NULL)
+    {
+        struct Node *temp = q->front;
+        q->front = q->front->next;
+        free(temp);
+    }
 }
---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// int main()
+// {
+//     static int var[5]; // array
+//     int count = 0;     // variable
+
+//     var[++count] = ++count;
+//     for (count = 0; count < 5; count++)
+//         printf("%d ", var[count]);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     char str[] = "value is =%d";
+//     int a = '7';
+//     // str[11] = 'c';
+//     printf(str, a);
+//     return 0;
+// }
